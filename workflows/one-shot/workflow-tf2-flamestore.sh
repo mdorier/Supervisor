79ce43cda63d3be3081389d431a85583e7367666
Matthieu Dorier <mdorier@anl.gov>
2020-03-07 17:53:27 +0000

    taking into account TURBINE_PRELAUNCH

--- workflows/one-shot/workflow-tf2-flamestore.sh
+++ workflows/one-shot/workflow-tf2-flamestore.sh
@@ -11,21 +11,22 @@ source $THIS/settings-flamestore.sh
 PROJECT_ROOT=/projects/radix-io/flamestore
 WORKSPACE=$PROJECT_ROOT/Supervisor/workflows/one-shot/flamestore-ws
 
+export PROJECT_ROOT
 export TURBINE_OUTPUT_ROOT=$THIS
 export TURBINE_OUTPUT_FORMAT="out-%Q"
 export TURBINE_JOBNAME=$JOB
 
-export TURBINE_PRELAUNCH=<<ENDOFDIRECTIVE
+TURBINE_PRELAUNCH="
 export MPICH_GNI_NDREG_ENTRIES=1024
 module swap PrgEnv-intel PrgEnv-gnu
 source $PROJECT_ROOT/spack/share/spack/setup-env.sh
 export MODULEPATH=$MODULEPATH:$PROJECT_ROOT/spack/share/spack/modules/cray-cnl6-mic_knl
 spack env activate flamestore
 spack load -r flamestore
-ENDOFDIRECTIVE
+"
 
-export QUEUE PROJECT WALLTIME
+export QUEUE PROJECT WALLTIME TURBINE_PRELAUNCH
 
 let NODES=WORKERS+1
 
-swift-t -m theta -n $NODES -e THIS $THIS/workflow-tf2-flamestore.swift --workspace=$WORKSPACE
+swift-t -m theta -n $NODES -e THIS -e TURBINE_PRELAUNCH $THIS/workflow-tf2-flamestore.swift --workspace=$WORKSPACE
